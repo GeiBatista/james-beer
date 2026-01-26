ef1ebe480872b532909160a6052d2b78335af074
geibatista <geibatista@hotmail.com>
2026-01-26 11:28:12 -0300

    combos de sabor e estilo e radio button de nacionalidade

--- src/main/java/com/mrbatista/jamesbeer/controller/CervejaController.java
+++ src/main/java/com/mrbatista/jamesbeer/controller/CervejaController.java
@@ -10,10 +10,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mrbatista.jamesbeer.model.Cerveja;
+import com.mrbatista.jamesbeer.model.Origem;
+import com.mrbatista.jamesbeer.model.Sabor;
 import com.mrbatista.jamesbeer.repository.Cervejas;
+import com.mrbatista.jamesbeer.repository.Estilos;
 
 @Controller
 public class CervejaController {
@@ -21,27 +25,26 @@ public class CervejaController {
 	private static final Logger  logger = LoggerFactory.getLogger(CervejaController.class);
 	
 	@Autowired Cervejas cervejas;
+	@Autowired Estilos estilos;
 	
 	@RequestMapping("/cervejas/novo")
-	private String novo(Cerveja cerveja) {
-		logger.error("Log nivel error!");
-		logger.info("Log nivel info!");
-		
-		cervejas.findAll();
-		return "cerveja/CadastroCerveja";
+	private ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		return mv;
 	}
 	
 	@PostMapping("/cervejas/novo")
-	private String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	private ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
-//			model.addAttribute(cerveja);
-//			model.addAttribute("mensagem", "Campo obrigatório");			
 		return novo(cerveja);
 		}
 		
 		attributes.addFlashAttribute("mensagem", "cerveja salva com sucesso!");
 		System.out.println("SKU: " +  cerveja.getSku());
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
 }
