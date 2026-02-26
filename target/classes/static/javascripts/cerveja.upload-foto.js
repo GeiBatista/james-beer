966d50a283167626943372e48f8fa655bad140d1
geibatista <geibatista@hotmail.com>
2026-02-25 21:17:35 -0300

    mantendo a foto ao validar os campos

--- target/classes/static/javascripts/cerveja.upload-foto.js
+++ target/classes/static/javascripts/cerveja.upload-foto.js
@@ -15,36 +15,39 @@ jamesbeer.UploadFoto = (function() {
     }
 
     UploadFoto.prototype.iniciar = function() {
-		var settings = {
+        var settings = {
             type: 'json',
             filelimit: 1,
             allow: '*.(jpg|jpeg|png)',
             action: this.containerFotoCerveja.data('url-fotos'),
-			complete: onUploadCompleto.bind(this)
-	}
-	
-	UIkit.uploadSelect($('#upload-select'), settings);
-	UIkit.uploadDrop(this.uploadDrop, settings);
-	
+            complete: onUploadCompleto.bind(this)
+        }
+
+        UIkit.uploadSelect($('#upload-select'), settings);
+        UIkit.uploadDrop(this.uploadDrop, settings);
+
+
+        if (this.inputNomeFoto.val()) {
+            onUploadCompleto.call(this, { nome: this.inputNomeFoto.val(), contentType: this.inputContentType.val() });
+        }
     }
-	
-	function onUploadCompleto(resposta) {
-		this.inputNomeFoto.val(resposta.nome);
-		this.inputContentType.val(resposta.contentType);
-		
-		this.uploadDrop.addClass('hidden');
-		var htmlFotoCerveja = this.template({nomeFoto: resposta.nome});
+    function onUploadCompleto(resposta) {
+        this.inputNomeFoto.val(resposta.nome);
+        this.inputContentType.val(resposta.contentType);
+
+        this.uploadDrop.addClass('hidden');
+        var htmlFotoCerveja = this.template({ nomeFoto: resposta.nome });
         this.containerFotoCerveja.append(htmlFotoCerveja);
-		
-		$('.js-remove-foto').on('click', onRemoverFoto.bind(this));
-	}
-	
-	function onRemoverFoto() {
-		$('.js-foto-cerveja').remove();
+
+        $('.js-remove-foto').on('click', onRemoverFoto.bind(this));
+    }
+
+    function onRemoverFoto() {
+        $('.js-foto-cerveja').remove();
         this.uploadDrop.removeClass('hidden');
         this.inputNomeFoto.val('');
         this.inputContentType.val('');
-	}
+    }
 
     return UploadFoto;
 })();
