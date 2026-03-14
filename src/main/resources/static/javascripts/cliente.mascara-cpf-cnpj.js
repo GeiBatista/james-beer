ad326e45e558155a53ac8e8ff6de3d134dda3f7e
geibatista <geibatista@hotmail.com>
2026-03-13 23:18:42 -0300

    Configurando máscara de CPF_CNPJ com jQuery

---
+++ src/main/resources/static/javascripts/cliente.mascara-cpf-cnpj.js
@@ -0,0 +1,31 @@
+JamesBeer = JamesBeer || {};
+
+JamesBeer.MaskCpfCnpj = (function() {
+
+    function MaskCpfCnpj() {
+		this.radioTipoPessoa = $('.js-radio-tipo-pessoa');
+		this.labelCpfCnpj = $('[for=cpfOuCnpj]');
+		this.inputCpfCnpj = $('#cpfOuCnpj');
+
+    }
+
+    MaskCpfCnpj.prototype.iniciar = function() {
+	    this.radioTipoPessoa.on('change', onTipoPessoaAlterado.bind(this));
+    }
+
+	function onTipoPessoaAlterado(event) {
+		var tipoPessoaSelecionada = $(event.currentTarget);
+		this.labelCpfCnpj.text(tipoPessoaSelecionada.data('documento'));
+		this.inputCpfCnpj.mask(tipoPessoaSelecionada.data('mascara'));
+		this.inputCpfCnpj.val('');
+		this.inputCpfCnpj.removeAttr('disabled');
+		}
+	
+    return MaskCpfCnpj;
+
+}());
+
+$(function() {
+	var maskCpfCnpj = new JamesBeer.MaskCpfCnpj();
+	maskCpfCnpj.iniciar();	
+});
