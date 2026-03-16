721ff3885989335921594be2467f87df9467a5dd
geibatista <geibatista@hotmail.com>
2026-03-16 18:31:44 -0300

    Selecionado cidades pelo estado com Ajax

--- src/main/java/com/mrbatista/jamesbeer/controller/CidadeController.java
+++ src/main/java/com/mrbatista/jamesbeer/controller/CidadeController.java
@@ -1,14 +1,33 @@
 package com.mrbatista.jamesbeer.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mrbatista.jamesbeer.model.Cidade;
+import com.mrbatista.jamesbeer.repository.Cidades;
 
 @Controller
+@RequestMapping("/cidades")
 public class CidadeController {
 	
-	@RequestMapping("/cidades/nova")
+	@Autowired
+	private Cidades cidades;
+	
+	@RequestMapping("/nova")
 	private String novo() {
 		return "cidade/CadastroCidade";
+	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(
+			@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado){		
+		return cidades.findByEstadoCodigo(codigoEstado);
 	}
 
 }
---
+++ src/main/java/com/mrbatista/jamesbeer/repository/Cidades.java
@@ -0,0 +1,15 @@
+package com.mrbatista.jamesbeer.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mrbatista.jamesbeer.model.Cidade;
+
+@Repository
+public interface Cidades extends JpaRepository<Cidade, Long>{
+	
+	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
+
+}
