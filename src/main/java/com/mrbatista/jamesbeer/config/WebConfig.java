5ef053c48af14cf610bff73904374814408b661a
geibatista <geibatista@hotmail.com>
2026-03-30 17:18:41 -0300

    cache profissional com guava do google

--- src/main/java/com/mrbatista/jamesbeer/config/WebConfig.java
+++ src/main/java/com/mrbatista/jamesbeer/config/WebConfig.java
@@ -2,11 +2,12 @@ package com.mrbatista.jamesbeer.config;
 
 import java.math.BigDecimal;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.BeansException;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -30,6 +31,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+import com.google.common.cache.CacheBuilder;
 import com.mrbatista.jamesbeer.controller.CervejaController;
 import com.mrbatista.jamesbeer.controller.converter.CidadeConverter;
 import com.mrbatista.jamesbeer.controller.converter.EstadoConverter;
@@ -110,6 +112,13 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
     
     @Bean
     public CacheManager cacheManager() {
-		return new ConcurrentMapCacheManager();
+		CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder()
+				.maximumSize(3)
+				.expireAfterAccess(5, TimeUnit.MINUTES);
+		
+		GuavaCacheManager cacheManager = new GuavaCacheManager();
+		cacheManager.setCacheBuilder(cacheBuilder);
+		return cacheManager;
+		
 	}
 }
