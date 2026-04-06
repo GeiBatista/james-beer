2b3b82a832e1d4bfe9b0e15fd4722acf92932d88
geibatista <geibatista@hotmail.com>
2026-04-06 18:34:27 -0300

    Escondendo componentes de usuários sem permissão

---
+++ src/main/java/com/mrbatista/jamesbeer/security/UsuarioSistema.java
@@ -0,0 +1,25 @@
+package com.mrbatista.jamesbeer.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import com.mrbatista.jamesbeer.model.Usuario;
+
+public class UsuarioSistema extends User{
+	
+	private static final long serialVersionUID = 1L;
+
+	private Usuario usuario;
+
+	public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+}
