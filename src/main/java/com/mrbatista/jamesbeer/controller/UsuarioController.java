fafc56276be5c21e8e678ffcf550d2b56484bb09
geibatista <geibatista@hotmail.com>
2026-04-08 19:34:01 -0300

    Multisseleção de linhas - ativandodesativando vários usuários

--- src/main/java/com/mrbatista/jamesbeer/controller/UsuarioController.java
+++ src/main/java/com/mrbatista/jamesbeer/controller/UsuarioController.java
@@ -3,11 +3,14 @@ package com.mrbatista.jamesbeer.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,6 +19,7 @@ import com.mrbatista.jamesbeer.repository.Grupos;
 import com.mrbatista.jamesbeer.repository.Usuarios;
 import com.mrbatista.jamesbeer.repository.filter.UsuarioFilter;
 import com.mrbatista.jamesbeer.service.CadastroUsuarioService;
+import com.mrbatista.jamesbeer.service.StatusUsuario;
 import com.mrbatista.jamesbeer.service.exception.SenhaObrigatoriaUsuarioException;
 import com.mrbatista.jamesbeer.service.exception.usuario.EmailUsuarioJaCadastradoException;
 
@@ -64,6 +68,12 @@ public class UsuarioController {
 		mv.addObject("usuarios", usuarios.filtrar(usuarioFilter));
 		mv.addObject("grupos", grupos.findAll());
 		return mv;
+	}
+	
+	@PostMapping("/status")
+	@ResponseStatus(HttpStatus.OK)
+	public void atualizarStatus(@RequestParam("codigos[]") Long[] codigos, @RequestParam("status") StatusUsuario statusUsuario) {
+		cadastroUsuarioService.alterarStatus(codigos, statusUsuario);
 	}
 
 }
